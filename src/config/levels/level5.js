b3c293c8ca1489eff5bd9de8901d7d740a25ba78
lehubduweb <guerrinflorian@yahoo.com>
2025-12-22 00:53:14 +0100

    feat: ajustement de la difficulté

--- src/config/levels/level5.js
+++ src/config/levels/level5.js
@@ -133,9 +133,9 @@ export const LEVEL_5 = {
       { count: 14, type: "ember_scout", interval: 380, startDelay: 8000 },
     ],
     [
-      { count: 4, type: "blackflame_sorcerer", interval: 3600, startDelay: 0 },
-      { count: 18, type: "scoria_soldier", interval: 700, startDelay: 0 },
-      { count: 6, type: "obsidian_bastion", interval: 2400, startDelay: 7000 },
+      { count: 6, type: "obsidian_bastion", interval: 2400, startDelay: 0 },
+      { count: 4, type: "blackflame_sorcerer", interval: 3600, startDelay: 7000 },
+      { count: 18, type: "scoria_soldier", interval: 700, startDelay: 13000 },
     ],
     [
       { count: 18, type: "ember_scout", interval: 340, startDelay: 0 },
@@ -165,7 +165,7 @@ export const LEVEL_5 = {
       { count: 10, type: "obsidian_bastion", interval: 2000, startDelay: 4000 },
       { count: 8, type: "blackflame_sorcerer", interval: 2500, startDelay: 6000 },
       { count: 6, type: "infernal_banner", interval: 2400, startDelay: 7000 },
-      { count: 1, type: "ash_queen", interval: 8000, startDelay: 12000 },
+      { count: 2, type: "ash_queen", interval: 8000, startDelay: 12000 },
       { count: 1, type: "bosslvl5", interval: 12000, startDelay: 20000 },
     ],
   ],
