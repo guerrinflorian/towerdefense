aba5f0d04a3578576ea2f644db21e21ef925d433
Florian <112929561+guerrinflorian@users.noreply.github.com>
2025-12-22 14:04:02 +0100

    refactor: organisation du contenu du chapitre 1

---
+++ src/config/ennemies/chapter1/index.js
@@ -0,0 +1,53 @@
+import { grunt } from "./grunt.js";
+import { runner } from "./runner.js";
+import { tank } from "./tank.js";
+import { shield } from "./shield.js";
+import { bosslvl1 } from "./bosslvl1.js";
+import { bosslvl2 } from "./bosslvl2.js";
+import { bosslvl3 } from "./bosslvl3.js";
+import { bosslvl4 } from "./bosslvl4.js";
+import { bosslvl5 } from "./bosslvl5.js";
+import { tortue_dragon } from "./tortue_dragon.js";
+import { shaman_gobelin } from "./shaman_gobelin.js";
+import { diviseur } from "./diviseur.js";
+import { slime_medium } from "./slime_medium.js";
+import { slime_small } from "./slime_small.js";
+import { cemeteryTank } from "./cemeteryTank.js";
+import { juggernaut_igloo } from "./juggernaut_igloo.js";
+import { soul_messenger_runner } from "./soul_messenger_runner.js";
+import { scoria_soldier } from "./scoria_soldier.js";
+import { ember_scout } from "./ember_scout.js";
+import { obsidian_bastion } from "./obsidian_bastion.js";
+import { infernal_banner } from "./infernal_banner.js";
+import { blackflame_sorcerer } from "./blackflame_sorcerer.js";
+import { ash_queen } from "./ash_queen.js";
+import { ash_egg } from "./ash_egg.js";
+import { flame_scarab } from "./flame_scarab.js";
+
+export const CHAPTER1_ENEMIES = {
+  grunt,
+  runner,
+  soul_messenger_runner,
+  tank,
+  shield,
+  bosslvl1,
+  bosslvl2,
+  bosslvl3,
+  bosslvl4,
+  bosslvl5,
+  tortue_dragon,
+  shaman_gobelin,
+  diviseur,
+  slime_medium,
+  slime_small,
+  cemeteryTank,
+  juggernaut_igloo,
+  scoria_soldier,
+  ember_scout,
+  obsidian_bastion,
+  infernal_banner,
+  blackflame_sorcerer,
+  ash_queen,
+  ash_egg,
+  flame_scarab,
+};
